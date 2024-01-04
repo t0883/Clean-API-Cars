ec7ba3e44a4f93421a5a77a690f0992ade076de6
t0883 <tobiiasa@hotmail.com>
2024-01-04 10:57:43 +0100

    SteeringWheel model that inherit from Brand model (#3)
    Co-authored-by: Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>

---
+++ Domain/Models/Spareparts/SteeringWheel.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Models.Brands;
+
+namespace Domain.Models.Spareparts
+{
+    public class SteeringWheel : Brand
+    {
+        public bool SteeringWheelHeater { get; set; }
+
+        public int SteeringWheelSize { get; set; }
+
+    }
+}
