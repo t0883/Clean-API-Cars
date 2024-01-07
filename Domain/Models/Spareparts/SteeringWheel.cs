40248ba8e22a55dd740b953ebb8041c4322e8661
Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>
2024-01-07 20:31:27 +0100

    Added sparepart model.

--- Domain/Models/Spareparts/SteeringWheel.cs
+++ Domain/Models/Spareparts/SteeringWheel.cs
@@ -4,6 +4,7 @@ namespace Domain.Models.Spareparts
 {
     public class SteeringWheel : Brand
     {
+        public Guid SteeringWheelId { get; set; }
         public bool SteeringWheelHeater { get; set; }
 
         public int SteeringWheelSize { get; set; }
