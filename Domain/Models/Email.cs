6e0fbd7ab9c2c6996e6b3ca3ae8e04ce84f07fec
t0883 <tobiiasa@hotmail.com>
2024-02-04 15:58:04 +0100

    Testat med email service (#12)
    Co-authored-by: Tobias Andersson <tobias.andersson@student.nbi-handelsakademin.se>

---
+++ Domain/Models/Email.cs
@@ -0,0 +1,12 @@
+﻿namespace Domain.Models
+{
+    public class Email
+    {
+        public string Subject { get; set; }
+
+        public string Body { get; set; }
+
+        public string From { get; set; }
+
+    }
+}
--- Infrastructure/EmailService/SendEmailService.cs
+++ Infrastructure/EmailService/SendEmailService.cs
@@ -1,21 +1,48 @@
-﻿using SendGrid;
+﻿using Domain.Models;
+using Microsoft.Extensions.Configuration;
+using SendGrid;
 using SendGrid.Helpers.Mail;
 
 namespace Infrastructure.EmailService
 {
     public class SendEmailService
     {
+        private readonly IConfiguration _configuration;
+
+        public SendEmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public async Task<Response> SendEmail()
         {
-            var apiKey = "HÄR SKA VARA EN API NYCKEL :)";
+            var apiKey = "";
+
+            var templateId = "d-48d99dea44d446579b58edd39b2da56e";
+
+            var apiKey1 = _configuration.GetConnectionString(apiKey);
+
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("HÄR SKA VARA EN VERIFIERAD EPOST ADRESS", "Example User");
+            var from = new EmailAddress("tumskruven@gmail.com");
             var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("test@example.com", "Example User");
+            var to = new EmailAddress("tobiiasa@hotmail.com");
             var plainTextContent = "and easy to do anywhere, even with C#";
             var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
-            var response = await client.SendEmailAsync(msg);
+
+            var randomstring = "";
+
+            Email emailObject = new Email();
+
+            emailObject.Subject = "Haj hajen";
+
+            emailObject.Body = "Detta är body";
+
+
+
+            var test = MailHelper.CreateSingleTemplateEmail(from, to, templateId, emailObject);
+
+            var response = await client.SendEmailAsync(test);
 
             return await Task.FromResult(response);
         }
